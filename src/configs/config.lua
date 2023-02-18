01656c9070bbcd552c3aa4049227f219844f25f2
maarsalien <osg1532@gmail.com>
2023-02-19 00:00:40 +0100

    🔥 remove code

--- src/configs/config.lua
+++ src/configs/config.lua
@@ -1,13 +1,10 @@
 local cfg = {}
 
-cfg.showUiButton = false
-
-cfg.VERSION_CODE = 2
-cfg.VERSION_NAME = "2.0.0"
-
-
 cfg.on    = "[✓]"
 cfg.off   = "[✗]"
 cfg.title = "Patcher v" .. cfg.VERSION_NAME
+
+
+cfg.showUiButton = false
 
 return cfg
